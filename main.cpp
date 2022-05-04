a1587cfcfdab5694aac90826b26ec7846339f763
Simone Andrea Cilia <ciliasimone22@gmail.com>
2022-05-04 18:36:37 +0200

    reset

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@ using namespace std;
 int main(){
 
     cout << "Hello world" << endl;
+<<<<<<< HEAD
 	EPIPNFioiofN
   	cout << " ciao mbare" << endl;
+=======
+  	cout << " Jamme ja" << endl;
+>>>>>>> parent of c512e51 ( parole a caso)
 }
